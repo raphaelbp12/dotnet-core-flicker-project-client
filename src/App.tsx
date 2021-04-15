9c60dd9e967bcd3e2557d20b3cee5157b3d99d1d
Raphael Parreira <rbp@blue-ocean-robotics.com>
2021-04-15 20:06:01 +0200

    feat: :sparkles: introduce material design; add chips component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { FeedAPI } from "./services/FeedService";
 import FeedItem from "./types/FeedItem";
 import FeedResult from "./types/FeedResult";
+import ChipInput from 'material-ui-chip-input'
 
 function App() {
-    const [query, setQuery] = useState<string>("f1,nature");
+    const queryDefaultValue : string[] = ['nature', 'f1'];
+    const [query, setQuery] = useState<string>(queryDefaultValue.join(','));
     const [items, setItems] = useState<FeedItem[]>([]);
 
     useEffect(() => {
-
         FeedAPI.search(query).then((result: FeedResult) => {
             console.log('result', result);
             if (result && result.items) {
@@ -18,9 +19,22 @@ function App() {
         });
     }, [query]);
 
-    return <div className="App">{ items.map((item: FeedItem) => {
-        return <img key={ item.link } src={ item.media.m } alt={ item.title } />
-    })}</div>;
+    const handleChange = useCallback((chips: string[]) => {
+        console.log('chips', chips.join(','));
+        setQuery(chips.join(','));
+    }, []);
+
+    return (
+        <div className="App">
+            <ChipInput
+                defaultValue={queryDefaultValue}
+                onChange={(chips) => handleChange(chips)}
+            />
+            { items.map((item: FeedItem) => {
+                return <img key={item.link} src={item.media.m} alt={item.title} />
+            })}
+        </div>
+    );
 }
 
 export default App;
